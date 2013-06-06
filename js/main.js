e034e722a6899d625272f83e004533d2e6a9d3da
Matthew Lewis <Matt@Matthews-MacBook-Pro.local>
2013-06-05 22:15:02 -0500

    Main page first commit
    Added initial rough html framework for the "main" app page (work in
    progress) as well as three initial functions for the various pages of
    the app into the main.js file.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ Matthew Lewis
 ASDI Term 1306
 main.js file
 */
+
+$('#main').on('pageinit', function(){
+
+
+});
+
+$('#add').on('pageinit', function(){
+	
+
+});
+
+$('#display').on('pageinit', function(){
+	
+
+});
