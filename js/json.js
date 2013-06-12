b507b0b4075f34051cb45452c34b21d6db7eb0c3
Matthew Lewis <Loneeagle14@aol.com>
2013-06-12 00:30:53 -0500

    Added default json games and autopopulate function
    Added default games to external json file and created a function to
    check local storage and if empty, pull games from json to populate the
    view page.

---
+++ js/json.js
@@ -0,0 +1,61 @@
+// Matthew Lewis
+// ASDI Term: 1306
+// JSON storage file 
+
+var json = {
+	"1123423": {
+		"name": ["Name: ", "Borderlands"],
+		"console": ["Console: ", "PC"],
+		"genre": ["Genre: ", "RPG"],
+		"bio": ["Bio: ", "Awesomeness in shooter/RPG form!"],
+		"favorite": ["Favorite: ", "Yes"],
+		"cloud": ["Cloud: ", ""]
+
+	},
+	"1234556": {
+		"name": ["Name: ", "Luigi's Mansion: Dark Moon"],
+		"console": ["Console: ", "3DS"],
+		"genre": ["Genre: ", "Adventure"],
+		"bio": ["Bio: ", "Followup title to the Gamecube original, Luigi's Mansion."],
+		"favorite": ["Favorite: ", "Yes"],
+		"cloud": ["Cloud: ", ""]
+
+	},
+	"987789": {
+		"name": ["Name: ", "Tomb Raider"],
+		"console": ["Console: ", "PC"],
+		"genre": ["Genre: ", "Action"],
+		"bio": ["Bio: ", "2012 remake of the original Tomb Raider series."],
+		"favorite": ["Favorite: ", "No"],
+		"cloud": ["Cloud: ", ""]
+
+	},
+	"63473": {
+		"name": ["Name: ", "Halo 4"],
+		"console": ["Console: ", "XBOX"],
+		"genre": ["Genre: ", "Action"],
+		"bio": ["Bio: ", "Another Halo game."],
+		"favorite": ["Favorite: ", "No"],
+		"cloud": ["Cloud: ", "Yes"]
+
+	},
+	"9282737": {
+		"name": ["Name: ", "Batman Arkham City"],
+		"console": ["Console: ", "PS3"],
+		"genre": ["Genre: ", "Action"],
+		"bio": ["Bio: ", "Batman...in all his glory."],
+		"favorite": ["Favorite: ", "Yes"],
+		"cloud": ["Cloud: ", ""]
+
+	},
+
+
+
+
+
+
+
+
+
+
+}
--- js/main.js
+++ js/main.js
@@ -34,9 +34,9 @@ $('#add').on('pageinit', function(){
 		game.console     = $("#console").val();
 		game.genre       = $("#genre").val();
 		game.bio         = $("#bio").val();
-		game.cloud       = $("#cloud").val();
 		game.favorite    = $("#favorite").val();
-	
+		game.cloud       = $("#cloud").val();
+
 		console.log(game);
 		//autofillData();
 		
@@ -67,8 +67,39 @@ $(reset).on("click", function(){
 $(display).on('click', function(){
 	if (localStorage.length == "0") {
 		alert("You haven't saved any games yet!  Default games will be generated for you.")
+		for(var n in json){
+				var id = Math.floor(Math.random()*100065);
+				localStorage.setItem(id, JSON.stringify(json[n]));
+		};
 
-
+		var createDiv = document.createElement('div');
+				createDiv.setAttribute("id", "items");
+				//var colorDiv = get("items");
+				//createDiv.appendChild(createList);
+				$('#display').append(createDiv);
+				for (var i=0, len=localStorage.length; i<len; i++) {
+					var createLi = document.createElement('li');
+					var createField = document.createElement('fieldset');
+					var createList = document.createElement('ul');
+					createDiv.appendChild(createField);
+					createField.appendChild(createList);
+					var createLinks = document.createElement('li');
+					createList.appendChild(createLi);
+					var key = localStorage.key(i);
+					var value = localStorage.getItem(key);
+					var obj = JSON.parse(value);
+					var createSubList = document.createElement('ul');
+					createLi.appendChild(createSubList);
+					//createImage(obj.origin[1], createSubList);
+					for(var n in obj) {
+						var createSubLi = document.createElement('li');
+						createSubList.appendChild(createSubLi);
+						var optSubText = obj[n][0]+" "+obj[n][1];
+						createSubLi.innerHTML = optSubText; 
+						createSubList.appendChild(createLinks);
+					}
+				createItemLinks(localStorage.key(i), createLinks);
+				}
 
 	} else {
 	var option = $(this).text();
@@ -88,6 +119,45 @@ function displayGames(option) {
 
 };
 	
+function createItemLinks(key, createLinks){
+			var edit = document.createElement('a');
+			edit.href = "#";
+			edit.key = key;
+			var txt = "Edit Member ";
+			edit.innerHTML = txt;
+			edit.addEventListener('click', editGame);
+			createLinks.appendChild(edit);
+			edit.style.display="block";
+			
+			var del = document.createElement('a');
+			del.href = '#';
+			del.key = key;
+			var delTxt = " Delete Member";
+			del.innerHTML = delTxt;
+			del.addEventListener('click', deleteGame);
+			createLinks.appendChild(del);
+		};
+
+function editGame() {
+	//alert("edit button clicked!");
+	$('#addLink').click();
+	var getKey = localStorage.getItem(this.key);
+			var edit = JSON.parse(getKey);
+			$("#name").val(edit.name[1]);
+			$("#console").html("<option>" + edit.console[1] + "</option>");
+			$("#genrePlaceholder").html(edit.genre[1]);
+			$("#bio").val(edit.bio[1]);
+			
+
+};
+function deleteGame() {
+	alert("delete button clicked!");
+
+};
+
+
+
+
 
 
 
