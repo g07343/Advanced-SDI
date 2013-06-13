31d830bd4c8c913b5b9adf8d9a41364ed449db1b
Matthew Lewis <Loneeagle14@aol.com>
2013-06-12 22:41:03 -0500

    Completed CRUD functionality
    Added in necessary code to enable complete CRUD functionality.  It's
    ugly as hell…but it works.  Form before function right??  right??!

--- js/main.js
+++ js/main.js
@@ -39,8 +39,10 @@ $('#add').on('pageinit', function(){
 					localStorage.setItem(id, JSON.stringify(game));
 
 					alert("Game Saved!");
+					$('#reset').click();
 					//$('#addGame').reset();
-					window.location.reload();
+					location.reload(true);
+					
 				}
 				function getFavorite(){
 					if ($('#favorite').is(':checked')){
@@ -108,26 +110,121 @@ $('#display').on('pageinit', function(){
 				}
 
 			function createItemLinks(key, createLinks){
-				var edit = document.createElement('a');
-				edit.href = "#";
-				edit.key = key;
-				var txt = "Edit Game ";
-				edit.innerHTML = txt;
-				//edit.addEventListener('click', editGame());
-				createLinks.appendChild(edit);
-				edit.style.display="block";
+				var editor = document.createElement('a');
+				editor.href = "#edit";
+				editor.key = key;
+				editor.class = "editLink";
+				var txt = "Edit Game";
+				editor.innerHTML = txt;
+				editor.addEventListener('click', editGame(key));
+				createLinks.appendChild(editor);
+				editor.style.display="block";
 				
 				var del = document.createElement('a');
 				del.href = '#';
 				del.key = key;
+				console.log(key);
 				var delTxt = " Delete Game";
 				del.innerHTML = delTxt;
 				//del.addEventListener('click', deleteGame());
 				createLinks.appendChild(del);
 			};
-});
+			function editGame(key){
+				var getKey = localStorage.getItem(key);
+				var game = JSON.parse(getKey);
+				var editor = $('.editLink');
+				//editor.href = "#edit";
+				//editor.click();
+				retrieveFav();
+				retrieveCloud();
 
-		
+
+
+				$("#editName").val(game.name[1]);
+				$("#editConsole").val(game.console[1]);
+				$("#editGenre").val(game.genre[1]);
+				$("#editBio").val(game.bio[1]);
+				$("#editCloud").val(cloudVal);
+
+				function retrieveFav(){
+					if(game.favorite[1] == "yes") {
+						//do this
+						console.log("HORRAAAY IT WAS CHECKED!");
+						$('#editFavorite').prop('checked', true);
+					} else {
+						return;
+					};
+
+				}
+				function retrieveCloud(){
+					if(game.cloud[1] == "on") {
+						//do this
+						console.log("HORRAAAY CLOUD WAS CHECKED!");
+						$('#editCloud').prop('checked', true);
+					} else {
+						return;
+					};
+
+				}
+			};
+			$('#submitEditedGame').on('click', function(){
+				console.log("Edited games key is " + key);
+				getFavorite();
+				getCloud();
+				if(!key){
+					var id = Math.floor(Math.random()*100065);
+					console.log(id);
+				}else{
+					id = key
+					
+				getFavorite();
+				getCloud();
+
+				var game            = {};
+					game.name       =["Name: ",    $("#editName").val()];
+					game.console    =["Console: ",    $("#editConsole").val()];
+					game.genre      =["Genre: ",     $("#editGenre").val()];
+					game.bio        =["Bio: ",    $("#editBio").val()];
+					game.favorite   =["Favorite? ",   favVal];
+					game.cloud      =["Cloud: ",   cloudVal];
+				
+					key = id;
+					
+					
+					localStorage.setItem(id, JSON.stringify(game));
+
+					alert("Game Saved!");
+					$('#reset').click();
+					$("#editHome").click();
+					location.reload(true);
+				};
+
+			});
+			
+			function getFavorite(){
+					if ($('#editFavorite').is(':checked')){
+						favVal = "yes";
+						
+					} else {
+						favVal = "no";
+						
+					}
+					console.log("favVal is " + favVal);
+					return favVal;
+				};
+				function getCloud(){
+					console.log("Cloud is" + $('#cloud').val());
+					if($('#editCloud').is(':checked')){
+						cloudVal = "on";
+
+					} else {
+						cloudVal = "off";
+					}
+					return cloudVal;
+				}
+
+
+});
 
 
 
@@ -136,10 +233,20 @@ $('#settings').on('pageinit', function(){
 });
 
 $('#edit').on('pageinit', function(){
+
+
 	
+	$('#submitEditedGame').on('click', function(){
+		$('#resetEdit').click();
+
+		console.log('edit form reset!');
+	});
 });
+
+
 
 ///adding outside listeners, variables, etc down below...
 //$('saveGame').on('click', addGame());
 var cloudVal;
 var favVal;
+//var edit = $('.edit').key;
